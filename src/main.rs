640290ae9af32360cdb3ba681bffbe4bdf81d234
Suphanat Chunhapanya <haxx.pop@gmail.com>
2022-12-20 01:46:36 +0700

    Change latency

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,32 @@ use libp2p::gossipsub::{
     ValidationMode,
 };
 use libp2p::{
-    gossipsub, identity, mdns, swarm::NetworkBehaviour, swarm::SwarmEvent, PeerId, Swarm,
+    gossipsub, identity, swarm::SwarmEvent, Multiaddr, PeerId, Swarm,
 };
+use libp2p::multiaddr::Protocol;
+use rand::prelude::*;
 use rand::Rng;
 use std::collections::hash_map::DefaultHasher;
 use std::error::Error;
 use std::hash::{Hash, Hasher};
+use std::net::SocketAddr;
+use std::net::ToSocketAddrs;
 use std::time::Duration;
+use chrono::Local;
+
+fn log(s: String) {
+    println!("{}: {}", Local::now(), s);
+}
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     // Create a random PeerId
     let local_key = identity::Keypair::generate_ed25519();
     let local_peer_id = PeerId::from(local_key.public());
-    println!("Local peer id: {local_peer_id}");
+    log(format!("Local peer id: {local_peer_id}"));
 
     // Set up an encrypted DNS-enabled TCP Transport over the Mplex protocol.
     let transport = libp2p::development_transport(local_key.clone()).await?;
-
-    // We create a custom network behaviour that combines Gossipsub and Mdns.
-    #[derive(NetworkBehaviour)]
-    struct MyBehaviour {
-        gossipsub: Gossipsub,
-        mdns: mdns::async_io::Behaviour,
-    }
 
     // To content-address message, we can take the hash of message and use it as an ID.
     let message_id_fn = |message: &GossipsubMessage| {
@@ -47,7 +49,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .expect("Valid config");
 
     // build a gossipsub network behaviour
-    let mut gossipsub = Gossipsub::new(MessageAuthenticity::Signed(local_key), gossipsub_config)
+    let mut gossipsub: Gossipsub = Gossipsub::new(MessageAuthenticity::Signed(local_key), gossipsub_config)
         .expect("Correct configuration");
 
     // Create a Gossipsub topic
@@ -57,50 +59,65 @@ async fn main() -> Result<(), Box<dyn Error>> {
     gossipsub.subscribe(&topic)?;
 
     // Create a Swarm to manage peers and events
-    let mut swarm = {
-        let mdns = mdns::async_io::Behaviour::new(mdns::Config::default())?;
-        let behaviour = MyBehaviour { gossipsub, mdns };
-        Swarm::with_async_std_executor(transport, behaviour, local_peer_id)
-    };
+    let mut swarm = Swarm::with_async_std_executor(transport, gossipsub, local_peer_id);
 
     // Listen on all interfaces and whatever port the OS assigns
-    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+    swarm.listen_on("/ip4/0.0.0.0/tcp/8080".parse()?)?;
 
     // Create the random number generator
     let mut rng = rand::thread_rng();
-    let mut interval = stream::interval(Duration::from_secs(4)).fuse();
+    let mut interval = stream::interval(Duration::from_secs(10)).fuse();
+
+    let hostname = nix::unistd::gethostname().unwrap().into_string().unwrap();
+    let host_idx = hostname[4..].parse().unwrap();
+    let num_peers: u16 = std::env::args().nth(1).unwrap().parse().unwrap();
+    let mut nums: Vec<u16> = (1..=num_peers).collect();
+    nums.shuffle(&mut rng);
+    let mut idx = 0;
+    let mut connected = 0;
+    while connected < 6 {
+        let addr = format!("{}{}", "peer", nums[idx]);
+        if addr == hostname {
+            idx += 1;
+            continue;
+        }
+        if nums[idx] < host_idx {
+            idx += 1;
+            connected += 1;
+            continue;
+        }
+        log(format!("connecting to {} from peer{}", &addr, host_idx));
+        let SocketAddr::V4(sock_addr) = (&addr[..], 8080).to_socket_addrs()?.next().unwrap() else { todo!() };
+
+        let mut remote: Multiaddr = Multiaddr::empty();
+        remote.push(Protocol::Ip4(*sock_addr.ip()));
+        remote.push(Protocol::Tcp(sock_addr.port()));
+        swarm.dial(remote)?;
+        connected += 1;
+        idx += 1;
+    }
 
     // Kick it off
     loop {
         select! {
             () = interval.select_next_some() => {
-                if let Err(e) = swarm
-                    .behaviour_mut().gossipsub
-                    .publish(topic.clone(), format!("Hello {} from {}", rng.gen::<u64>(), local_peer_id)) {
-                    println!("Publish error: {e:?}");
+                if host_idx == 1 {
+                    if let Err(e) = swarm
+                        .behaviour_mut()
+                        .publish(topic.clone(), format!("Hello {:08x} from {}", rng.gen::<u32>(), local_peer_id)) {
+                        log(format!("Publish error: {e:?}"));
+                    }
                 }
             },
             event = swarm.select_next_some() => match event {
-                SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
-                    for (peer_id, _multiaddr) in list {
-                        println!("mDNS discovered a new peer: {peer_id}");
-                        swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
-                    }
-                },
-                SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
-                    for (peer_id, _multiaddr) in list {
-                        println!("mDNS discover peer has expired: {peer_id}");
-                        swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
-                    }
-                },
-                SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(GossipsubEvent::Message {
-                    propagation_source: peer_id,
-                    message_id: id,
+                SwarmEvent::Behaviour(GossipsubEvent::Message {
+                    propagation_source: _peer_id,
+                    message_id: _id,
                     message,
-                })) => println!(
-                        "Got message: '{}' with id: {id} from peer: {peer_id}",
+                }) => log(format!(
+                        "Got message: '{}'",
                         String::from_utf8_lossy(&message.data),
-                    ),
+                    )),
                 _ => {}
             }
         }
